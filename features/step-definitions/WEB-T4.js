00f8c5b989feaec61a2029960acda272a44380c1
AshvinGitHub <92999777+AshvinGitHub@users.noreply.github.com>
2023-07-23 15:46:07 +0530

    Update WEB-T4.js

--- features/step-definitions/WEB-T4.js
+++ features/step-definitions/WEB-T4.js
@@ -2,19 +2,19 @@ import { Given, When, Then } from '@wdio/cucumber-framework';
 
 Given(/^I am on the login page$/, () => {
     // [Given] Sets up the initial state of the system.   
-	console.log('I am on the login page')
+	console.log("I am on the login page")
 
 });
 
 When(/^I login with (\w+) and (.+)$/, (username, password) => {
 
     // [When] Describes the action or event that triggers the scenario.
-		console.log('Enter the username and password')
+		console.log("Enter the username and password")
 
 });
 
 Then(/^I should see a flash message saying (.*)$/, (message) => {
     // [Then] Describes the expected outcome or result of the scenario.
-		    console.log('I should see a flash message saying)
+		    console.log("I should see a flash message saying")
 
 });
