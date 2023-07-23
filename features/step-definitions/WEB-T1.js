7fdf35f8b22c45b957d03b31db32b7bb0363c7a5
AshvinGitHub <92999777+AshvinGitHub@users.noreply.github.com>
2023-07-23 14:18:50 +0530

    Update WEB-T1.js

--- features/step-definitions/WEB-T1.js
+++ features/step-definitions/WEB-T1.js
@@ -1,24 +1,24 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
-Given(`login into cargo site`, () => {
+Given(/^login into cargo site$/, () => {
     // [Given] Sets up the initial state of the system.
 	 console.log('login into cargo site')
 
 });
 
-Then(`Login Successfully`, () => {
+Then(/^Login Successfully$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 		 console.log('Login Successfully')
 
 });
 
-When(`Nevigate to Create Sell Price`, () => {
+When(/^Nevigate to Create Sell Price$/, () => {
     // [When] Describes the action or event that triggers the scenario.
 			 console.log(`Nevigate to Create Sell Price`)
 
 });
 
-When(`Create the Sell Price`, () => {
+When(/^Create the Sell Price$/, () => {
     // [When] Describes the action or event that triggers the scenario.
 				 console.log(`Create the Sell Price`)
 
