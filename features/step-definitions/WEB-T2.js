b90a65a2ec590514ec087babc87d4a4a16adae28
AshvinGitHub <92999777+AshvinGitHub@users.noreply.github.com>
2023-07-23 14:20:16 +0530

    Update WEB-T2.js

--- features/step-definitions/WEB-T2.js
+++ features/step-definitions/WEB-T2.js
@@ -1,18 +1,18 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
-Given(`Go to login Page`, () => {
+Given(/^Go to login Page$/, () => {
     // [Given] Sets up the initial state of the system.
     console.log('Go to login Page')
 
 });
 
-When(`Enter the (\w+) and (.+)}`, (username, password) => {
+When(/^Enter the (\w+) and (.+)}$/, (username, password) => {
     // [When] Describes the action or event that triggers the scenario.
 	    console.log('Enter the username and password')
 
 });
 
-Then(`User Login successfully`, () => {
+Then(/^User Login successfully$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 		    console.log('User Login successfully')
 
