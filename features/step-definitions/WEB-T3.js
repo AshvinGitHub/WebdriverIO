336caccb59b880a46b8d45d5bddc68bff5d3e2bc
AshvinGitHub <92999777+AshvinGitHub@users.noreply.github.com>
2023-07-23 14:22:47 +0530

    Update WEB-T3.js

--- features/step-definitions/WEB-T3.js
+++ features/step-definitions/WEB-T3.js
@@ -1,25 +1,25 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
-Given(`Go to login Page`, () => {
+Given(/^Go to login Page$/, () => {
     // [Given] Sets up the initial state of the system.
 });
 
-When(`Enter the (\w+) and (.+)}`, (username, password) => {
+When(/^Enter the (\w+) and (.+)$/, (username, password) => {
     // [When] Describes the action or event that triggers the scenario.
 });
 
-Then(`User Login successfully`, () => {
+Then(/^User Login successfully$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 });
 
-Then(`Click on Price`, () => {
+Then(/^Click on Price$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 });
 
-Then(`Click on Sell Price`, () => {
+Then(/^Click on Sell Price$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 });
 
-Then(`Click on Create`, () => {
+Then(/^Click on Create$/, () => {
     // [Then] Describes the expected outcome or result of the scenario.
 });
